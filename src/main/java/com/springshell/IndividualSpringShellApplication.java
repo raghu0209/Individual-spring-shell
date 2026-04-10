35de310f24cd61e393143823985cb0cea00eaef9
Raghavendra Dasari <162073459+raghu0209@users.noreply.github.com>
2026-04-10 22:01:11 +0530

    Add files via upload

---
+++ src/main/java/com/springshell/IndividualSpringShellApplication.java
@@ -0,0 +1,13 @@
+package com.springshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualSpringShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualSpringShellApplication.class, args);
+	}
+
+}
